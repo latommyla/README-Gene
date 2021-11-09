67a44e5b5e64b32166aad23ec9daee934f11c485
Tommy La <tommyl.dmd@gmail.com>
2021-11-08 21:22:07 -0800

    added README questions

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # README-Gene
+
 Homework #9 
---
+++ index.js
@@ -0,0 +1,59 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project title?"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Description of your project?"
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "How to install your project?"
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What is the projects usage?"
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "What is the projects license?"
+  },
+  {
+    type: "input",
+    name: "contribute",
+    message: "Who contributed to the project?"
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What is the projects test?"
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "What is your GitHub user name?"
+  },
+  {
+    type: "input",
+    name: "repo",
+    mesage: "What is your GitHub repo link?"
+  },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
